2bb3434a3262e6c3fb5b7891ce74f753079f4bd8
sahil202005 <sahilbidwai20@gmail.com>
2026-06-11 20:04:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/sales-analytics-dashboard/',
   plugins: [
     react(),
     tailwindcss(),
